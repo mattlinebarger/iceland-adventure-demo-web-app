c330d26d9aa0fe57fcb0a10b75f280bb879b57ba
Matt Linebarger <mlinebarger@pagerduty.com>
2023-08-23 10:36:49 -0500

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+function _ready(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+      fn();
+    } else {
+      document.addEventListener("DOMContentLoaded", fn);
+    }
+  }
